ee5a16b5791260644644ce7dc0eaa229310fa40b
Juan Diaz <juanjoc2000@hotmail.com>
2023-09-21 06:03:55 -0300

    feat create formUsers

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,12 @@
+'use client'
 import Cards from "@/components/cards/cards";
+import { useEffect } from "react";
 
 const Home = () => {
+    useEffect(() => {
+        console.log(process.env.FIREBASE_API_KEY);
+        
+    },[])
     return (
         <div className="flex">
             <Cards/>
---
+++ app/register/page.tsx
@@ -0,0 +1,10 @@
+import Register from "@/components/forms/register/Register"
+const pageRegister = () => {
+    return (
+        <div>
+            <Register/>
+        </div>
+    )
+}
+
+export default pageRegister
