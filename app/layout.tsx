f2313123f3e106638157e1548b4c73760a8c0930
Uriel Campos <urielcampos448@gmail.com>
2023-09-21 03:30:02 -0300

    Initial commit from Create Next App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
-import { Providers } from '@/lib/providers'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
+const inter = Inter({ subsets: ['latin'] })
 
+export const metadata: Metadata = {
+  title: 'Discount Dash',
+  description: 'Tu app de ofertas',
+}
 
-export default function RootLayout(props: React.PropsWithChildren) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <Providers>
-      <html lang="en">
-        <body>
-          {props.children}
-        </body>
-      </html>
-    </Providers>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }
