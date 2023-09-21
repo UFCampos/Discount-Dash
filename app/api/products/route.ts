7ead0dc6f8b3dacd4851870edc407569218fff2b
Uriel Campos <urielcampos448@gmail.com>
2023-09-20 21:35:22 -0300

    refactor[api/products]: removed commented snippet

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -2,13 +2,6 @@ import { NextResponse, NextRequest } from "next/server";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "@/firebase/config";
 
-// export const GET = async () => {
-//     const productsSnapshot = await getDocs(collection(db, "products")); 
-//     const products = productsSnapshot.docs.map((doc) => doc.data());
-//     return NextResponse.json(products)
-// }
-
-
 export const GET = async (req: NextRequest) => {
     const name = req.nextUrl.searchParams.get("name");
   
