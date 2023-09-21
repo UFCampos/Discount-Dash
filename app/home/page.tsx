e41b7ad3a59ed547e83ce4286631f244ada85d13
Juan Diaz <juanjoc2000@hotmail.com>
2023-09-21 17:04:17 -0300

    fix [firebase/config]: made env variables public

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,7 @@
-'use client'
 import Cards from "@/components/cards/cards";
 import { useEffect } from "react";
 
 const Home = () => {
-    useEffect(() => {
-        console.log(process.env.FIREBASE_API_KEY);
-        
-    },[])
     return (
         <div className="flex">
             <Cards/>
