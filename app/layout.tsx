ec17751297714f9d1ecfb4705e15e264bd5156c1
Uriel Campos <urielcampos448@gmail.com>
2023-09-21 04:09:07 -0300

    feat[api]: added product search and post routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,12 @@ import './globals.css'
 import type { Metadata } from 'next'
 import NavBar from '@/components/navBar/NavBar'
 
+
 export const metadata: Metadata = {
   title: 'Discount Dash',
   description: 'Tu app de ofertas',
 }
+
 export default function RootLayout(props: React.PropsWithChildren) {
   return (
     <Providers>
