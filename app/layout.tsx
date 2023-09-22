15ae19f8d0bb546d018fdcbb85a205a9cdad800c
Uriel Campos <urielcampos448@gmail.com>
2023-09-21 21:02:21 -0300

    fix[layout.tsx]: navbar not rendering

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Providers } from '@/lib/providers'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import NavBar from '@/components/navBar/NavBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout(props: React.PropsWithChildren) {
     <Providers>
       <html lang="en">
         <body className='red-text'>
+          <NavBar />
           {props.children}
         </body>
       </html>
