39f442adfee9c9eb512a14a2c4283430058183c6
Juan Diaz <juanjoc2000@hotmail.com>
2023-09-24 03:19:22 -0300

    Update Profile

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -1,16 +1,29 @@
 'use client'
 import { useGetProfileQuery } from "@/lib/redux/service/searchProfileAPI"
+import Image from "next/image"
 
-const Profile = (id : Number) => {
+type ProfileProps = {
+    id : string
+}
 
-    const { data } = useGetProfileQuery({id})
+const Profile: React.FC<ProfileProps> = ({id}) => {
 
+    // const { data } = useGetProfileQuery({id})
+    const data = {
+        name: "Juan",
+        image: "/henry.jpg",
+    }
 
     return(
-        <div>
+    <div className="flex flex-col items-center justify-center border-black">
             <h1>Profile</h1>
             <p>This is the profile page</p>
             <div>
+                <Image src={data?.image} width={200} height={200} alt={data?.name} className="rounded border-black"/>
+                <h1>{data?.name}</h1>
+                <div>
+                    
+                </div>
 
             </div>
         </div>
