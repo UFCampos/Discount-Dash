c82a72572f197cc5706ec3e2dcec72e274187494
Uriel Campos <urielcampos448@gmail.com>
2023-09-25 03:06:14 -0300

    feat[users]: added route to post users

---
+++ app/api/users/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "@/firebase/config";
+
+export const GET = async () => {
+    const usersSnapshot = await getDocs(collection(db, "users"));
+
+    const users = usersSnapshot.docs.map((doc) => doc.data());
+
+    return NextResponse.json(users);
+}
