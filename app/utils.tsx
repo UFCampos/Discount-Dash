5d2bce5d6b426191e9c3ffd3a7573182e3f8a7d4
Juan Diaz <juanjoc2000@hotmail.com>
2023-09-26 18:27:19 -0300

    Feat: [Menu and profile]

--- app/utils.tsx
+++ app/utils.tsx
@@ -1,19 +1,24 @@
 import { GoogleAuthProvider, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
 import { auth } from "@/firebase/config";
-const googleProvider= new GoogleAuthProvider();
-const facebookProvider= new FacebookAuthProvider();
 
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
-export const signInProvider = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const value: string = event.currentTarget.value;
-    console.log(value)
-    signInWithPopup(auth, value === "google" ? googleProvider : facebookProvider)
-        .then((response) => {
-            console.log(response.user);
-        })
-        .catch((error) => {
-            alert(error.message)
-        }
+export const signInProvider = async (event: React.MouseEvent<HTMLButtonElement>) => {
+  const value: string = event.currentTarget.value;
 
-        )
-} 
+  try {
+    const response = await signInWithPopup(auth, value === "google" ? googleProvider : facebookProvider);
+    const user = {
+      id: response.user.uid,
+      email: response.user.email,
+      photoUrl: response.user.photoURL,
+      name: response.user.displayName
+    };
+    console.log(user);
+    return user;
+  } catch (error) {
+    alert(error.message);
+    return null; // Devuelve null en caso de error
+  }
+};
