febc918aeea9292cacda64f1fb990744366929a1
Uriel Campos <urielcampos448@gmail.com>
2023-09-26 03:34:26 -0300

    feat[cart-users]: created cart route And fixed register user route

---
+++ app/api/cart/add/[id]/route.ts
@@ -0,0 +1,24 @@
+import { db } from "@/firebase/config";
+import { addDoc, collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { NextResponse, NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest, {params}: {params: {id: string}}) => {
+    // Add a document to the "cart" subcollection of "user"
+    const {userId, itemId} = await req.json();
+
+    const cartId = params.id;
+
+    console.log(cartId);
+    
+    if (userId && cartId) {
+        const cartRef = doc(db, "users/" + userId + "/cart/" + cartId);
+        const item = await getDoc(doc(db, "products/" + itemId));
+        await addDoc(collection(db, `users/${userId}/cart`), {
+            ...item.data(),
+            id: item.id
+        });
+        return NextResponse.json(item.data());
+    }
+
+    return NextResponse.json({error: "Not found"}, {status: 404});
+}
--- app/api/cart/add/route.ts
+++
@@ -1,3 +0,0 @@
-import { db } from "@/firebase/config";
-import { collection, getDocs } from "firebase/firestore";
-import { NextResponse } from "next/server";
