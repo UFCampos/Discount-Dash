342a6a7611b6a14007f34cc350f213bddee48ff6
CMCarlaMecatti <canmecatti@gmail.com>
2023-09-27 18:39:49 -0300

    feat[routes]: Added put routes. Added put routes for cart and product.

---
+++ app/api/products/putProduct/controller.ts
@@ -0,0 +1,20 @@
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "@/firebase/config";
+
+export const controller = async (
+  productId: string,
+  stock: number,
+  name: string,
+  brand: string,
+  price: string,
+  image: string
+) => {
+  const docRef = doc(db, "products", productId);
+  await updateDoc(docRef, {
+    stock: stock,
+    name: name,
+    brand: brand,
+    price: price,
+    image: image,
+  });
+};
---
+++ app/api/products/putProduct/updateStock/updateStock.ts
@@ -0,0 +1,20 @@
+import { doc, updateDoc, increment, getDoc } from "firebase/firestore";
+import { db } from "@/firebase/config";
+import { handler } from "./handler";
+
+export const updateStock = async (
+  userId: string,
+  cartItemId: string,
+  value: string
+) => {
+  const refCartItem = doc(db, "users", userId, "cart", cartItemId);
+  const cartStock = (await getDoc(refCartItem)).get("quantity");
+  const refProduct = doc(db, "products", cartItemId);
+  if (!value) {
+    await updateDoc(refProduct, {
+      stock: increment(-cartStock),
+    });
+  } else {
+    handler(refProduct, value);
+  }
+};
