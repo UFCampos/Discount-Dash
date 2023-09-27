de68cf439346c58416397f57b36d373e7dc2fdba
Uriel Campos <urielcampos448@gmail.com>
2023-09-27 19:23:24 -0300

    feat[api/cart]: finished GET and POST routes

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -4,15 +4,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req : NextRequest) => {
     const userId : any = req.nextUrl.searchParams.get("id");
-    const cartRef = doc(collection(db, "users", userId, "cart"));
-    const {id} = await getDoc(cartRef);
-
-    console.log(id);
-    
-    const itemSnapshot = await getDocs(collection(db, "users", userId, "cart", "49fyGk4egg8Q0rqossyn", "items"));
-
+    const itemSnapshot = await getDocs(collection(db, "users", userId, "cart"));
+ 
     //get all items from the items collection
-    const items = itemSnapshot.docs.map((doc) => doc.data());
+    const items = itemSnapshot.docs.map((doc) => {
+        return {
+            ...doc.data(),
+            id: doc.id
+        }
+    });
     
     return NextResponse.json(items);
 }
