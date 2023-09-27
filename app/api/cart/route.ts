03847a86b9a27cf578ec41510521cf54358e7443
Uriel Campos <urielcampos448@gmail.com>
2023-09-27 12:55:18 -0300

    feat[api/cart]: added get cart items route

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,10 +1,18 @@
 import { db } from "@/firebase/config";
-import { collection, getDocs } from "firebase/firestore";
-import { NextResponse } from "next/server";
+import { collection, getDocs, doc, getDoc } from "firebase/firestore";
+import { NextRequest, NextResponse } from "next/server";
 
-export const getCart = async () => {
-    //FALTA LA REFERENCIA A USER
-    const cartSnapshot = await getDocs(collection(db, "cart"));
-    const cart = cartSnapshot.docs.map((doc) => doc.data());
-    return NextResponse.json(cart);
+export const GET = async (req : NextRequest) => {
+    const userId : any = req.nextUrl.searchParams.get("id");
+    const cartRef = doc(collection(db, "users", userId, "cart"));
+    const {id} = await getDoc(cartRef);
+
+    console.log(id);
+    
+    const itemSnapshot = await getDocs(collection(db, "users", userId, "cart", "49fyGk4egg8Q0rqossyn", "items"));
+
+    //get all items from the items collection
+    const items = itemSnapshot.docs.map((doc) => doc.data());
+    
+    return NextResponse.json({cart: items});
 }
