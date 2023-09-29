465dcfaa72bca7bc891141c834f4b260b65c3403
Uriel Campos <urielcampos448@gmail.com>
2023-09-29 09:19:53 -0300

    feat[auth]: completed authentication

--- app/utils.tsx
+++ app/utils.tsx
@@ -4,21 +4,8 @@ import { auth } from "@/firebase/config";
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
 
-export const signInProvider = async (event: React.MouseEvent<HTMLButtonElement>) => {
+export const signInProvider = (event: React.MouseEvent<HTMLButtonElement>) => {
   const value: string = event.currentTarget.value;
-
-  try {
-    const response = await signInWithPopup(auth, value === "google" ? googleProvider : facebookProvider);
-    const user = {
-      id: response.user.uid,
-      email: response.user.email,
-      photoUrl: response.user.photoURL,
-      name: response.user.displayName
-    };
-    console.log(user);
-    return user;
-  } catch (error) {
-    alert(error.message);
-    return null; // Devuelve null en caso de error
-  }
+  const provider = value === "google" ? googleProvider : facebookProvider;
+  signInWithPopup(auth, provider)
 };
--- components/forms/login/Login.tsx
+++ components/forms/login/Login.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { auth } from "@/firebase/config";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { signInProvider } from "@/app/utils";
 import { setUser } from "@/lib/redux/features/userProfile";
-import { useDispatch } from "@/lib/redux/hooks";
+import { useDispatch, useSelector } from "@/lib/redux/hooks";
 
 const Login = () => {
+  // const [user, newSetUser] = useState(null)
+  const user = useSelector((state) => state.userProfile);
   const dispatch = useDispatch();
 
   const [formData, setFormData] = useState({
@@ -25,32 +27,9 @@ const Login = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     signInWithEmailAndPassword(auth, formData.email, formData.password)
-      .then((response) => {
-        dispatch(
-          setUser({
-            id: response.user.uid,
-            email: response.user.email,
-            photoUrl: response.user.photoURL,
-            name: response.user.displayName,
-          })
-        );
-      })
       .catch((error) => {
         alert(error.message);
       });
-  };
-
-  const handlerClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
-    const value = event;
-    const data = await signInProvider(value);
-    dispatch(
-      setUser({
-        id: data.id,
-        email: data.email,
-        photoUrl: data.photoUrl,
-        name: data.name,
-      })
-    );
   };
 
   return (
@@ -81,12 +60,12 @@ const Login = () => {
         </div>
       </form>
       <div>
-        <button onClick={handlerClick} value={"google"}>
+        <button onClick={(e) => signInProvider(e)} value={"google"}>
           Sign in with Google
         </button>
       </div>
       <div>
-        <button onClick={signInProvider} value={"facebook"}>
+        <button onClick={(e) => signInProvider(e)} value={"facebook"}>
           Sign in with Facebook
         </button>
       </div>
