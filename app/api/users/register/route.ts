39576cb0211410a1a633740599ad6b66a7154378
Federico Omar Farfan <132364592+fof3096@users.noreply.github.com>
2023-09-29 16:41:29 -0300

    Revert "Dev federico"

--- app/api/users/register/route.ts
+++ app/api/users/register/route.ts
@@ -2,13 +2,14 @@ import { NextResponse, NextRequest } from "next/server";
 import { controller } from "./createUser";
 
 export const POST = async (req: NextRequest) => {
-  const { name, lastname, email } = await req.json();
+    const { name, lastname, email } = await req.json();
 
-  if (!name || !lastname || !email) {
-    return NextResponse.json({ error: "Insufficient data" }, { status: 400 });
-  }
+    if (!name || !lastname || !email) {
+        return NextResponse.json({ error: "Insufficient data" }, {status: 400 });
+    }  
 
-  await controller(name, lastname, email);
+    await controller(name, lastname, email);
+    
 
-  return NextResponse.json({ message: "User added" });
-};
+    return NextResponse.json({message: 'User added'});
+}
