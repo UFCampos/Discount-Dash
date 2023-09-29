eb813627bb2b7ce451b4b4bfcdf2014e40e320f5
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2023-09-29 09:55:29 -0300

    Update store.ts

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -7,7 +7,6 @@ import { userProfileSlice } from "./features/userProfile";
 import { cartProductsAPI } from "./service/cartProductsAPI";
 import { addProductCartSlice } from "./features/addProductCartSlice";
 import { productsApi } from './service/productsAPI'
-import { searchProfileAPI } from './service/searchProfileAPI'
 import {userApi} from './service/usersRegisterAPI'
 
 export const store = configureStore({
@@ -20,7 +19,6 @@ export const store = configureStore({
         [searchProfileAPI.reducerPath] : searchProfileAPI.reducer,
         [userApi.reducerPath]:userApi.reducer,
         [cartProductsAPI.reducerPath]: cartProductsAPI.reducer,
-        [searchProfileAPI.reducerPath]: searchProfileAPI.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat( productsApi.middleware, userApi.middleware, cartProductsAPI.middleware,),
 })
