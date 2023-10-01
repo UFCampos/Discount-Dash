f6231e794aeeceae0f44267f5f719f9645d112cb
Uriel Campos <urielcampos448@gmail.com>
2023-10-01 17:31:55 -0300

    fix[productByName]: added id to results

--- app/api/products/productByName.ts
+++ app/api/products/productByName.ts
@@ -1,10 +1,16 @@
 import { collection, getDocs, where, query } from "firebase/firestore"
 import { db } from "@/firebase/config"
+import { Product } from "@/utils/types";
 
 export const productByName = async (name: string) => {
   
     const productsSnapshot = await getDocs(query(collection(db, "products"), where("nameToLowerCase", ">=", name.toLowerCase()), where("nameToLowerCase", "<=", name.toLowerCase() + "\uf8ff")));
-    const products = productsSnapshot.docs.map((doc) => doc.data());
+    const products: Product[] = productsSnapshot.docs.map((doc) => (
+      {
+        id: doc.id,
+        ...(doc.data() as Product),
+      }
+    ));
     if (products.length === 0) {
       return {error: "Product not found" , status: 404};
     }
