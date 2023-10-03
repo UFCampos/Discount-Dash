f1704dab3123829f57b24e50f361d29778e8c386
CMCarlaMecatti <canmecatti@gmail.com>
2023-10-03 17:36:09 -0300

    Commit only for personal pull

--- app/api/users/register/createUser.ts
+++ app/api/users/register/createUser.ts
@@ -1,8 +1,9 @@
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
 
-export const controller = async (name: string, lastname: string, email: string) => {
-    addDoc(collection(db, "users"), {
+export const controller = async (id: string, name: string, lastname: string, email: string) => {
+    const userRef = doc(db, "users", id);
+    setDoc(userRef, {
         name,
         lastname,
         email
