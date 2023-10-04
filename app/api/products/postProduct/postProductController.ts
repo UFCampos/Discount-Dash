4c5a1d795f607f2917bc5f63e185bec75d691894
CMCarlaMecatti <canmecatti@gmail.com>
2023-10-04 13:56:44 -0300

    fix[back]: Fixed add and update routes

--- app/api/products/postProduct/postProductController.ts
+++ app/api/products/postProduct/postProductController.ts
@@ -1,20 +1,29 @@
-import { addDoc, collection, Timestamp  } from "firebase/firestore"
-import { db } from "@/firebase/config"
+import { addDoc, collection, Timestamp } from "firebase/firestore";
+import { db } from "@/firebase/config";
 
-export const controller = (name: string, brand: string, price:string, stock:string, image:string, originalPrice:string, expirationDate:string, userId:string) => {
-    const expirationTimestamp = Timestamp.fromDate(new Date(expirationDate))
-    
-    
-    addDoc(collection(db, "products"), {
-        userId,
-        name,
-        nameToLowerCase: name.toLowerCase(),
-        brand,
-        originalPrice: parseInt(originalPrice),
-        price: parseInt(price),
-        stock: parseInt(stock),
-        expirationDate: expirationTimestamp,
-        image
+export const controller = (
+  name: string,
+  brand: string,
+  price: string,
+  stock: string,
+  image: string,
+  normalPrice: string,
+  expiration: string,
+  userId: string,
+  description: string
+) => {
+  const expirationTimestamp = Timestamp.fromDate(new Date(expiration));
 
-    })
-}
+  addDoc(collection(db, "products"), {
+    userId,
+    name,
+    nameToLowerCase: name.toLowerCase(),
+    brand,
+    normalPrice: parseInt(normalPrice),
+    price: parseInt(price),
+    stock: parseInt(stock),
+    expiration: expirationTimestamp,
+    image,
+    description,
+  });
+};
