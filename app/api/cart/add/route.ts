fb5f2ce1fa8e5fe53a0d740ee3b0d77be919c6df
UrielC2004 <131821174+UFCampos@users.noreply.github.com>
2023-10-06 13:42:42 -0300

    Dev federico (#92)
    * feat[routes]: route deleteProduct added
    
    * feat[app]: can be purchased using buy a product
    >failures: stock does not decrease when purchasing
    
    * feat[app]: The card "buy" button now reduces the stock of the purchased product.
    
    >The "payProduct" route is not necessary at the moment, I upload it just in case.
    
    ---------
    
    Co-authored-by: fof3096 <c0rr1f1ll@gmail.com>

--- app/api/cart/add/route.ts
+++ app/api/cart/add/route.ts
@@ -1,23 +1,33 @@
 import { db } from "@/firebase/config";
-import { addDoc, collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
 import { NextResponse, NextRequest } from "next/server";
 
-export const POST = async (req: NextRequest, {params}: {params: {id: string}}) => {
-    // Add a document to the "cart" subcollection of "user"
-    const { userId, itemId } = await req.json();
+export const POST = async (req: NextRequest) => {
+  // Add a document to the "cart" subcollection of "user"
+  const { userId, itemId } = await req.json();
 
-    if (userId && itemId) {
-        const product = await getDoc(doc(db, "products", itemId));
+  if (userId && itemId) {
+    const product = await getDoc(doc(db, "products", itemId));
 
-        const cartItemCollection = collection(db, "users", userId, "cart")
+    const cartItemCollection = collection(db, "users", userId, "cart");
 
-        const item = await setDoc(doc(cartItemCollection, itemId), {
-            ...product.data(),
-            quantity: 1
-        });
-        
-        return NextResponse.json({message: "Producto añadido con éxito al carrito"},{status: 201});
-    }
+    const item = await setDoc(doc(cartItemCollection, itemId), {
+      ...product.data(),
+      quantity: 1,
+    });
 
-    return NextResponse.json({error: "Not found"}, {status: 404});
-}
+    return NextResponse.json(
+      { message: "Producto añadido con éxito al carrito" },
+      { status: 201 }
+    );
+  }
+
+  return NextResponse.json({ error: "Not found" }, { status: 404 });
+};
