d31e446d485bcfd869b72330473ac1df2c9502a3
Uriel Campos <urielcampos448@gmail.com>
2023-10-09 11:57:31 -0300

    fix

--- app/api/products/buyProduct/route.ts
+++ app/api/products/buyProduct/route.ts
@@ -6,6 +6,7 @@ export const POST = async (req: NextRequest) => {
     access_token:
       "TEST-4954619061793476-100604-94b7ec6da1dcee7f7e008fe698837e3a-1501138541",
   });
+
   const { itemId, description, price, quantity } = await req.json();
 
   let preference = {
@@ -19,9 +20,9 @@ export const POST = async (req: NextRequest) => {
     ],
 
     back_urls: {
-      success: `${process.env.SUCCES_ROUTE}`,
-      failure: `${process.env.SUCCES_ROUTE}`,
-      pending: `${process.env.SUCCES_ROUTE}`,
+      success: `https://discount-dash-53vw-lcuojigzb-ufcampos.vercel.app/home`,
+      failure: `https://discount-dash-53vw-lcuojigzb-ufcampos.vercel.app/home`,
+      pending: `https://discount-dash-53vw-lcuojigzb-ufcampos.vercel.app/home`,
     },
     auto_return: "approved",
   };
