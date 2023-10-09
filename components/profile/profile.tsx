3085be06e0ab19870d38454055d0a1b863a3d783
Juan Diaz <juanjoc2000@hotmail.com>
2023-10-09 18:16:50 -0300

    fix[Profile]: added styles profile with google/Fb.

--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -14,29 +14,55 @@ const Profile: React.FC<ProfileProps> = ({ id }) => {
 
   if (isError) {
     return (
-        <div className="flex flex-col items-center justify-center border-black">
-          <h1>Profile</h1>
-          <p>This is the profile page</p>
-          <div className={style.container}>
-            <div className={style.perfil}>
-              <Image
-                src={user?.photoUrl ? user?.photoUrl : "/default.jpg"}
-                width={350}
-                height={350}
-                alt={user?.name}
-                className={style.fotoPerfil}
-              />
-              <h1 className={style.name}>{user?.name}</h1>
-              <p className={style.email}>{user?.email}</p>
+      <section className="pt-16 bg-blueGray-50">
+      <div className="w-full lg:w-4/12 px-4 mx-auto">
+        <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-16 shadow-pink-400">
+          <div className="px-6">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full px-4 flex justify-center">
+                <div className="relative  items-center flex">
+                  <Image alt="img"
+                    src={user?.photoUrl ? user?.photoUrl : "/default.jpg"} width={300} height={300} className="shadow-xl rounded-full h-auto align-middle border-none w-40 -mt-20  " />
+                </div>
+              </div>
+
             </div>
-            <div className={style.configuracion}>
-              <h2>configuration</h2>
-              <p>segurity</p>
-              <p>pepito</p>
-              <p>pepita</p>
+            <div className="text-center">
+              <h3 className="text-xl font-semibold leading-normal mb-2 text-blueGray-700">
+                {user?.name}
+              </h3>
+              <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+                {user?.email}
+              </div>
+              <div className="mb-2 text-blueGray-600">
+                <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
+                info
+              </div>
+            </div>
+            <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+              <div className="flex flex-wrap justify-center">
+                <div className="w-full lg:w-9/12 px-4">
+                  <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    configuration
+                  </p>
+                  <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    segurity
+                  </p>
+                  <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    pepito
+                  </p>
+                  <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    pepita
+                  </p>
+
+                </div>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+    </section>
     );
   }
 
