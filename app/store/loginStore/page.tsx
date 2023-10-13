0875c765c2e26db3a2ce598c1c9a5874726b203a
thomasrey99 <thomas_rey1999@outlook.com>
2023-10-12 22:15:12 -0300

    <feats>[login store]:add functionalities for login store form

--- app/store/loginStore/page.tsx
+++ app/store/loginStore/page.tsx
@@ -7,13 +7,25 @@ import { useState, useEffect } from "react"
 import loginValidation from "./validations/loginValidation"
 import { loginErrorStore, login } from "@/utils/types"
 import React from "react"
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/firebase/config"
+import { useDispatch } from "@/lib/redux/hooks"
+import { setUser } from "@/lib/redux/features/userProfile"
+import { useRouter } from "next/navigation"
 
 const LoginStore=()=>{
 
+    const router=useRouter()
+
+    const dispatch=useDispatch()
+     
     const [loginInfo, setLoginInfo]=useState<login>({
         email:"",
         password:""
     })
+
+    const [uid, setUid] = useState("");
+
     const [loginErrorStore, setErrors]=useState<loginErrorStore>({
         emptyEmail:"",
         invalidEmail:"",
@@ -35,9 +47,44 @@ const LoginStore=()=>{
             [name]:value
         }))
     }
+
+    const handleSubmit=async (event: React.FormEvent<HTMLFormElement>)=>{
+        event.preventDefault()
+        await signInWithEmailAndPassword(auth, loginInfo.email, loginInfo.password)
+        .then(userCredential=>{
+            const {user}=userCredential
+            const {uid}=user
+            setUid(uid)
+            const mappedUser = {
+                id: uid,
+                email: user.email,
+                photoUrl: user.photoURL,
+                name: user.displayName,
+            }
+            dispatch(setUser(mappedUser));
+            router.push(`/store/${uid}`)
+		})
+		.catch(error => {
+			console.error(error);
+		})
+    }
     useEffect(()=>{
         setErrors(loginValidation(loginInfo))
-    }, [loginInfo])
+        onAuthStateChanged(auth, (user)=>{
+            if(user){
+                let mappedUser = {};
+                user.providerData.forEach((profile) => {
+                    mappedUser = {
+                        id: uid ? uid : profile.uid, // Usar el uid del estado
+                        email: profile.email,
+                        photoUrl: profile.photoURL,
+                        name: profile.displayName,
+                    };
+                });
+                dispatch(setUser(mappedUser));
+            }
+        })
+    }, [loginInfo, uid, dispatch])
 
     return (
         <main className={style.main}>
@@ -49,7 +96,7 @@ const LoginStore=()=>{
                 <section className={style.formCont}>
                     <h1>log in</h1>
                     <div className={style.loginCard}>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className={style.formGroup}>
                                 <label htmlFor="email">Email</label>
                                 <input name="email" id="username" type="email" value={loginInfo.email} onChange={handleChange}/>
