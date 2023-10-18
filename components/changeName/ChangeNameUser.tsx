74a8e371cfd385170b0b95f0221639ad49056051
thomasrey99 <thomas_rey1999@outlook.com>
2023-10-18 05:24:05 -0300

    <refactor>[change name]:add styles for change name form

--- components/changeName/ChangeNameUser.tsx
+++ components/changeName/ChangeNameUser.tsx
@@ -2,6 +2,7 @@
 import { useChangeNameMutation } from "@/lib/redux/service/changeNameAPI";
 import { useSelector } from "@/lib/redux/hooks";
 import { useState } from "react";
+import style from "./ChangeName.module.css"
 
 const ChangeNameUser = () => {
   const [name, setName] = useState("");
@@ -17,10 +18,16 @@ const ChangeNameUser = () => {
     });
   };
   return(
-    <div>
-        <h1>Change name</h1>
-        <input type="text" onChange={(e) => setName(e.target.value)}/>
-        <button onClick={sendNewName}>Change name</button>
+    <div className={style.cont}>
+      <div className={style.card}>
+        <span className={style.cardTitle}>change your name</span>
+        <p className={style.cardContent}>
+        </p>
+        <div className={style.cardForm}>
+            <input placeholder="Your name" type="text" onChange={(e) => setName(e.target.value)}/>
+            <button className={style.changeButton}onClick={sendNewName}>Change name</button>
+        </div>
+      </div>
     </div>
   )
 };
