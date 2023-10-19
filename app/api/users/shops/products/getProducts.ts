86040f6ae3fe3fb78652732bbfd800c5fc3c51e7
CMCarlaMecatti <canmecatti@gmail.com>
2023-10-19 10:27:09 -0300

    Fix & Contact

--- app/api/users/shops/products/getProducts.ts
+++ app/api/users/shops/products/getProducts.ts
@@ -1,11 +1,12 @@
 import {db} from '@/firebase/config';
 import {collection, query, where, getDocs} from 'firebase/firestore';
 import { type Products, Product } from '@/utils/types';
+import { logicalDelete } from '@/app/api/products/deleteProduct/logicalDelete';
 
 export const handler = async (shopId: string, value: string) => {
 	const productsRef = collection(db, 'products');
 	let productsQuery = query(productsRef, where('shopId', '==', shopId));
-
+    await logicalDelete();
 	if (value === 'expired') {
 		productsQuery = query(productsQuery, where('status', '==', value));
 	}
---
+++ components/forms/contact/Contact.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from 'react';
+import style from './contact.module.css';
+
+const Contact=() =>{
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+  };
+
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default Contact;
