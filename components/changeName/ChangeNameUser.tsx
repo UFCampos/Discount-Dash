c32b1a9225049e02b845fcced3db330b30e8d48c
fof3096 <c0rr1f1ll@gmail.com>
2023-10-19 11:57:16 -0300

    refactor[components/ChangNameUser]: popup message.

--- components/changeName/ChangeNameUser.tsx
+++ components/changeName/ChangeNameUser.tsx
@@ -2,37 +2,42 @@
 import { useChangeNameMutation } from "@/lib/redux/service/changeNameAPI";
 import { useSelector } from "@/lib/redux/hooks";
 import { useState } from "react";
-import style from "./ChangeName.module.css"
+import style from "./ChangeName.module.css";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const ChangeNameUser = () => {
-
   const [name, setName] = useState("");
   const { id } = useSelector((state) => state.userProfile);
   const [mutate] = useChangeNameMutation();
-  const router=useRouter()
+  const router = useRouter();
 
-  const sendNewName = async() => {
-    console.log(id, name)
+  const sendNewName = async () => {
+    toast.success("Name changed");
     await mutate({
-      userId:id,
-      name:name,
+      userId: id,
+      name: name,
     });
-    router.back()
+    router.back();
   };
-  return(
+  return (
     <div className={style.cont}>
       <div className={style.card}>
         <span className={style.cardTitle}>change your name</span>
-        <p className={style.cardContent}>
-        </p>
+        <p className={style.cardContent}></p>
         <div className={style.cardForm}>
-            <input placeholder="Your name" type="text" onChange={(e) => setName(e.target.value)}/>
-            <button className={style.changeButton} onClick={sendNewName}>Change name</button>
+          <input
+            placeholder="Your name"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className={style.changeButton} onClick={sendNewName}>
+            Change name
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default ChangeNameUser;
