eb6e5b22daf18bc64ed8b90e76453cec241370ff
fof3096 <c0rr1f1ll@gmail.com>
2023-10-20 00:30:47 -0300

    refactor[redux/features]: Kill "console.log"

--- lib/redux/features/cartItemsSlice.ts
+++ lib/redux/features/cartItemsSlice.ts
@@ -1,37 +1,37 @@
-import {createSlice} from '@reduxjs/toolkit';
-import {type CartProduct} from '@/utils/types';
+import { createSlice } from "@reduxjs/toolkit";
+import { type CartProduct } from "@/utils/types";
 
 type CartItems = {
-	cartItems: CartProduct[];
-	cartItemsId: Record<string, number>;
+  cartItems: CartProduct[];
+  cartItemsId: Record<string, number>;
 };
 
 const initialState: CartItems = {
-	cartItems: [],
-	cartItemsId: {},
+  cartItems: [],
+  cartItemsId: {},
 };
 
 export const cartItemsSlice = createSlice({
-	name: 'cartItems',
-	initialState,
-	reducers: {
-		addCart(state, {payload}) {
-			const hasItem = state.cartItems.find(item => item.id === payload.id);
-			if (hasItem) {
-				hasItem.quantity = (hasItem.quantity || 0) + 1;
-			} else {
-				payload = {
-					...payload,
-					quantity: 1,
-				};
-				console.log(payload);
-				state.cartItems.push(payload);
-			}
-		},
-		addTotalCart(state, {payload}) {
-			state.cartItems = payload;
-		},
-	},
+  name: "cartItems",
+  initialState,
+  reducers: {
+    addCart(state, { payload }) {
+      const hasItem = state.cartItems.find((item) => item.id === payload.id);
+      if (hasItem) {
+        hasItem.quantity = (hasItem.quantity || 0) + 1;
+      } else {
+        payload = {
+          ...payload,
+          quantity: 1,
+        };
+
+        state.cartItems.push(payload);
+      }
+    },
+    addTotalCart(state, { payload }) {
+      state.cartItems = payload;
+    },
+  },
 });
 
-export const {addCart, addTotalCart} = cartItemsSlice.actions;
+export const { addCart, addTotalCart } = cartItemsSlice.actions;
