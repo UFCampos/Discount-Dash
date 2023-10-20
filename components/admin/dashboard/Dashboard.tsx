c5dd24086f182c0cc818cd6a4a73e64cfa02c643
fof3096 <c0rr1f1ll@gmail.com>
2023-10-19 23:18:10 -0300

    refactor[components/dashboard]: kill "console.log"

--- components/admin/dashboard/Dashboard.tsx
+++ components/admin/dashboard/Dashboard.tsx
@@ -1,35 +1,34 @@
-'use client'
+"use client";
 import { useSelector } from "@/lib/redux/hooks";
 import { useState } from "react";
 import { Button } from "@nextui-org/react";
 import Sidebar from "./Sidebar";
 import Users from "./Users";
 import { useGetAuthUserQuery } from "@/lib/redux/service/searchProfileAPI";
-import style from "./Dashboard.module.css"
+import style from "./Dashboard.module.css";
 import { Image } from "@nextui-org/react";
 import LoadingComponent from "@/components/loading/Loading";
 const Dashboard = () => {
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const { data: user, isLoading } = useGetAuthUserQuery(null);
-    console.log(user);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { data: user, isLoading } = useGetAuthUserQuery(null);
 
-    if (isLoading) {
-        return <LoadingComponent/>;
-    }
-    if (user?.customClaims?.admin) {
-        return (
-            <div className={style.adminDashCont}>
-                    <Sidebar/>
-                    <Users/>
-            </div>
-        );
-    } else {
-        return (
-            <div>
-                <Image src="/padlock.png" alt='padlock screen'/>
-            </div>
-        );
-    }
-}
+  if (isLoading) {
+    return <LoadingComponent />;
+  }
+  if (user?.customClaims?.admin) {
+    return (
+      <div className={style.adminDashCont}>
+        <Sidebar />
+        <Users />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Image src="/padlock.png" alt="padlock screen" />
+      </div>
+    );
+  }
+};
 
 export default Dashboard;
